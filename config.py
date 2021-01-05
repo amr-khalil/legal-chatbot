7cfdcfe06dc7672854e8fc9d6e27015d0e66eaa3
Amr Khalil <44541932+amr-khalil@users.noreply.github.com>
2021-01-06 00:43:03 +0100

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ class Config(object):
     # Data encryption 
     SECRET_KEY = os.environ.get('SECRET_KEY') or '*********'  
     # Database 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'chatbot.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     # Pagination
