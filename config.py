e1837911966ffe9f8e4f2389bb3b0d85221fec45
Amr Khalil <44541932+amr-khalil@users.noreply.github.com>
2021-01-06 00:24:48 +0100

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     # Data encryption 
     SECRET_KEY = os.environ.get('SECRET_KEY') or '*********'  
     # Database 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = postgres://cqetmnwtscwkma:a4158dfd4adfe6374ac58ac3a2783a4213220dfdc02b00c7d502932f1b5715d8@ec2-54-216-202-161.eu-west-1.compute.amazonaws.com:5432/d2i2ieck1govpj
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     # Pagination
